d29fed5d8808cbe99f304d937f76eea1c2c6a49d
Majd <majdhasanalstef@gmail.com>
2025-02-04 22:47:53 +0300

    make Migration Filles

--- database/migrations/2025_02_04_193147_create_activities_table.php
+++ database/migrations/2025_02_04_193147_create_activities_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('activities', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin', 'volunteer'])->default('volunteer');
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('description');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('location');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('activites');
     }
 };
--- database/migrations/2025_02_04_194413_create_gallery_table.php
+++ database/migrations/2025_02_04_194413_create_gallery_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('gallery', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin', 'volunteer'])->default('volunteer');
-            $table->rememberToken();
+            $table->foreignId('activity_id')->constrained('activities')->onDelete('cascade');
+            $table->enum('media_type', ['image', 'video']);
+            $table->string('media_url');
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('gallery');
     }
 };
